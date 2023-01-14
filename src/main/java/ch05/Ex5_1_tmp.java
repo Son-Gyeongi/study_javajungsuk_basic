daa931fcd9f4944bea906fa9361536700021fe56
Son-Gyeongi <thsruddl77@gmail.com>
2023-01-14 12:01:45 +0900

    chapter5 배열의 출력 Arrays.toString

--- src/main/java/ch05/Ex5_1_tmp.java
+++ src/main/java/ch05/Ex5_1_tmp.java
@@ -1,8 +1,10 @@
 package ch05;
 
+import java.util.Arrays;
+
 public class Ex5_1_tmp {
     public static void main(String[] args) {
-        /*
+        /* 배열 선언과 생성
 //        int[] score; // 1. 배열 score를 선언 (참조변수 선언)
 //        score = new int[5]; // 2. 배열의 생성 (int 저장공간 x 5)
 
@@ -18,6 +20,7 @@ public class Ex5_1_tmp {
         System.out.println("value= "+value);
          */
 
+        /*
         // index 범위 : 0~4
         int[] arr = new int[5]; // 길이가 5인 int 배열 arr을 생성
         System.out.println(arr.length);
@@ -25,5 +28,13 @@ public class Ex5_1_tmp {
         for (int i=0; i<arr.length; i++) {
             System.out.println("arr["+i+"] = " + arr[i]);
         }
+         */
+
+        int[] iArr = {100,95,80,70,60}; // 길이가 5인 int 배열
+        for (int i=0; i < iArr.length;i++) {
+            System.out.println(iArr[i]);
+        }
+
+        System.out.println(Arrays.toString(iArr));
     }
 }
