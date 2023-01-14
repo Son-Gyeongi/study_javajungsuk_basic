50a987e960e4ea1fb7505dc95dbda39c20db1cfa
Son-Gyeongi <thsruddl77@gmail.com>
2023-01-14 15:20:02 +0900

    chapter5 String 배열의 초기화 / 가위, 바위, 보 문자열 랜덤으로 출력

--- src/main/java/ch05/Ex5_1_tmp.java
+++ src/main/java/ch05/Ex5_1_tmp.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class Ex5_1_tmp {
     public static void main(String[] args) {
-        // Ex5_5
         // index: 0~3-1, 0~2
         String[] strArr = {"가위", "바위", "보"};
         System.out.println(Arrays.toString(strArr));
