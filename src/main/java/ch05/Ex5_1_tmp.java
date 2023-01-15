a30c7a512b9faf1592a6dc0f42f01c4fe92c0df2
Son-Gyeongi <thsruddl77@gmail.com>
2023-01-15 21:04:38 +0900

    chapter5 Arrays 클래스들의 메서드들

--- src/main/java/ch05/Ex5_1_tmp.java
+++ src/main/java/ch05/Ex5_1_tmp.java
@@ -4,13 +4,41 @@ import java.util.Arrays;
 
 public class Ex5_1_tmp {
     public static void main(String[] args) {
-        // String 클래스 메서드들
+        // Arrays 클래스들의 메서드들
+        int[] arr = {0,1,2,3,4}; // 1차원 배열 arr
+        int[][] arr2D = {{11,12}, {21,22}}; // 2차원 배열 arr2D
+
+        // 배열 출력
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.deepToString(arr2D));
+
+        // 배열 비교
+        String[][] str2D = {{"aaa","bbb"},{"AAA","BBB"}};
+        String[][] str2D2 = {{"aaa","bbb"},{"AAA","BBB"}};
+
+        System.out.println(str2D==str2D2); // 참조변수 값 비교
+        System.out.println(Arrays.deepEquals(str2D, str2D2));
+
+        // 배열 복사
+        int[] arr2 = Arrays.copyOf(arr, 7);
+        System.out.println(Arrays.toString(arr2));
+
+        int[] arr3 = Arrays.copyOfRange(arr, 2,4);
+        System.out.println(Arrays.toString(arr3));
+
+        // 배열 정렬
+        int[] arr4 = {1,4,3,0,2};
+        Arrays.sort(arr4); // 배열 arr을 오름차순으로 정렬
+        System.out.println(Arrays.toString(arr4));
+
+        /* String 클래스 메서드들
         String str = "ABCDE";
         char ch = str.charAt(2);
         String str2 = str.substring(1, 4); // 결과 : BCD
         String str3 = str.substring(1); // 1~4 결과 : BCDE (1) == (1, str.length())
         String str4 = str.substring(1, str.length());
         System.out.println(str4);
+         */
 
         /* 배열의 인덱스 범위
         // index: 0~3-1, 0~2
