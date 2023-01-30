3a7cbac283150f518ec67ce412911d6a3cc81cbe
Son-Gyeongi <thsruddl77@gmail.com>
2023-01-30 17:58:17 +0900

    chapter5 연습문제 6_2

--- src/main/java/ch06/Practice6_2.java
+++ src/main/java/ch06/Practice6_2.java
@@ -35,7 +35,7 @@ class Student6_2 {
 
 public class Practice6_2 {
     public static void main(String[] args) {
-        Student s = new Student("홍길동", 1, 1, 100, 60, 75);
+        Student6_2 s = new Student6_2("홍길동", 1, 1, 100, 60, 75);
 
         String str = s.info();
         System.out.println(str);
