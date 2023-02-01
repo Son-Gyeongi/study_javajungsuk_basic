496a9c95d3283242ed1ed3cb269edba3ad50de1d
Son-Gyeongi <thsruddl77@gmail.com>
2023-02-01 17:50:01 +0900

    chapter6 연습문제 6_21

--- src/main/java/ch06/Practice6_21.java
+++ src/main/java/ch06/Practice6_21.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Practice6_21 {
     public static int abs(int value) {
 //        return Math.abs(value);
+
         // 답안
         return value >= 0 ? value : -value;
     }
