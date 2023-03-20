154a5d36f86bf5038784018f39e4720726579bf5
Son-Gyeongi <thsruddl77@gmail.com>
2023-03-21 08:32:26 +0900

    chapter7 인터페이스 미사용 시, 새로운 클래스 C를 만들고 사용 시 변경이 편리하지 않다.

--- src/main/java/ch07/InterfaceTest.java
+++ src/main/java/ch07/InterfaceTest.java
@@ -2,7 +2,7 @@ package ch07;
 
 class A {
     // B의 메서드를 호출하는 method
-    public void method(B b) {
+    public void method(C b) {
         b.method();
     }
 }
@@ -13,9 +13,15 @@ class B {
     }
 }
 
+class C {
+    public void method() {
+        System.out.println("C 클래스의 메서드");
+    }
+}
+
 public class InterfaceTest {
     public static void main(String[] args) {
         A a = new A();
-        a.method(new B()); // A가 B를 사용 (의존)
+        a.method(new C()); // A가 B를 사용 (의존)
     }
 }
