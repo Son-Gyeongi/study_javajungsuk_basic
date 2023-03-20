23378624cffed80505d63358991362263dda85f9
Son-Gyeongi <thsruddl77@gmail.com>
2023-03-21 08:47:08 +0900

    chapter7 인터페이스의 장점, 코드 변경 편리

--- src/main/java/ch07/InterfaceTest.java
+++ src/main/java/ch07/InterfaceTest.java
@@ -2,18 +2,24 @@ package ch07;
 
 class A {
     // B의 메서드를 호출하는 method
-    public void method(C b) {
-        b.method();
+    public void method(I i) { // 인터페이스 I를 구현한 놈들만 들어와라
+        i.method();
     }
 }
 
-class B {
+// B클래스의 선언과 구현을 분리
+
+interface I {
+    public void method();
+}
+
+class B implements I {
     public void method() {
         System.out.println("B 클래스의 메서드");
     }
 }
 
-class C {
+class C implements I {
     public void method() {
         System.out.println("C 클래스의 메서드");
     }
