a741b926ed76436a5422389b2d88225118bb0085
Son-Gyeongi <thsruddl77@gmail.com>
2023-03-22 16:09:40 +0900

    chapter7 연습문제 7_3 컴파일 에러가 나는 이유 - 해결

--- src/main/java/ch07/Practice7_3.java
+++ src/main/java/ch07/Practice7_3.java
@@ -4,22 +4,24 @@ class Product7_3 {
     int price; // 제품의 가격
     int bonusPoint; // 제품구매 시 제공하는 보너스점수
 
+    Product7_3() {} // Product 클래스에 기본생성자가 있어야 한다.
+
     Product7_3(int price) {
         this.price = price;
         bonusPoint = (int) (price/10.0);
     }
 }
 
-class Tv7 extends Product7_3 {
-    Tv7() {}
+class Tv7_3 extends Product7_3 {
+    Tv7_3() {}
 
     public String toString() {
-        return "Tv";
+        return "Tv7_3";
     }
 }
 
 public class Practice7_3 {
     public static void main(String[] args) {
-        Tv7 t = new Tv7();
+        Tv7_3 t = new Tv7_3();
     }
 }
