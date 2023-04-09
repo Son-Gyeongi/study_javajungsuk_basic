e59de7222cc7748ac2b40f28cdc63e4c2f575694
Son-Gyeongi <thsruddl77@gmail.com>
2023-04-09 23:42:57 +0900

    chapter8 checked 예외와 unchecked 예외

--- src/main/java/ch08/Ex8_7.java
+++ src/main/java/ch08/Ex8_7.java
@@ -6,5 +6,14 @@ public class Ex8_7 {
         // 에러 error: unreported exception java.lang.Exception; must be caught or declared to be thrown
         // 컴파일 에러, 컴파일조차 안된다.
         // Exception과 자손은 예외처리 필수 -> try-catch문 필수
+
+        // checked 예외는 try-catch문을 써줘야 컴파일 에러가 나지 않는다.
+        // Exception과 그 자손은 반드시 예외처리를 해줘야한다. (필수)
+        try {
+            throw new Exception();
+        } catch (Exception e) {}
+
+        // RuntimeException과 그 자손은 예외처리를 해주지 않아도 컴파일이 된다. (선택)
+        throw new RuntimeException();
     }
 }
--- src/main/java/ch08/Ex8_8.java
+++ src/main/java/ch08/Ex8_8.java
@@ -3,7 +3,17 @@ package ch08;
 public class Ex8_8 {
     public static void main(String[] args) {
         throw new RuntimeException(); // RuntimeException을 고의로 발생시킨다.
-        // 성공적으로 컴파일은 된다.
+        // 성공적으로 컴파일은 된다. 컴파일이 잘 된다고 해서 실행했을 때 예외가 발생하지 않는게 아니다.
         // RuntimeException과 자손은 예외처리 선택 -> try-catch문 선택
+
+        /**
+         * 에러가 안 나게 예외처리 예시
+         * try {
+         *      throw new RuntimeException();
+         *  } catch (Exception e) {
+         *
+         *  }
+         */
+
     }
 }
