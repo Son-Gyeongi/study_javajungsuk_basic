2f08d083c028fce0560dcb964e495d4f71ef519a
Son-Gyeongi <thsruddl77@gmail.com>
2023-04-09 16:54:36 +0900

    chapter8 try블럭 내에서 예외가 발생하지 않은 경우

---
+++ src/main/java/ch08/Ex8_1.java
@@ -0,0 +1,15 @@
+package ch08;
+
+// try블럭 내에서 예외가 발생하지 않은 경우
+public class Ex8_1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        try {
+            System.out.println(2);
+            System.out.println(3);
+        } catch (Exception e) {
+            System.out.println(4); // 실행되지 않는다
+        } // try-catch의 끝
+        System.out.println(5);
+    }
+}
