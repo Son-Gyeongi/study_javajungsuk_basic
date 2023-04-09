5757ed75dc370e382991ac609f471de4c3b4884a
Son-Gyeongi <thsruddl77@gmail.com>
2023-04-09 17:05:43 +0900

    chapter8 try블럭 내에서 예외가 발생한 경우 - 예외가 발생한 후의 문장은 실행되지 않는다.

--- src/main/java/ch08/Ex8_2.java
+++ src/main/java/ch08/Ex8_2.java
@@ -5,8 +5,8 @@ public class Ex8_2 {
     public static void main(String[] args) {
         System.out.println(1);
         try {
-            System.out.println(0/0); // 0으로 나눠서 고의로 ArithmeticException 산술계산 예외를 발생시킨다.
-            System.out.println(2);
+            System.out.println(0/0); // 예외발생!!! 0으로 나눠서 고의로 ArithmeticException 산술계산 예외를 발생시킨다.
+            System.out.println(2); // 실행되지 않는다. 예외가 발생한 후의 문장은 실행되지 않는다.
         } catch (ArithmeticException ae) {
             System.out.println(3);
         } // try-catch의 끝
