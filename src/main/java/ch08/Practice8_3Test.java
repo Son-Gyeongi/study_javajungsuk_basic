d5250f9ccb92d1f93049cb6970225face1e81822
Son-Gyeongi <thsruddl77@gmail.com>
2023-04-16 16:50:15 +0900

    chapter8 Practice8_2, Practice8_3

---
+++ src/main/java/ch08/Practice8_3Test.java
@@ -0,0 +1,33 @@
+package ch08;
+
+public class Practice8_3Test {
+    class NumberException extends Exception {}
+    class InvalidNumberException extends NumberException {}
+    class NotANumberException extends NumberException {}
+
+    class Parent {
+        int a;
+        int b;
+
+        Parent() {
+            this(0, 0);
+        }
+
+        Parent(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        void add(int a, int b) throws InvalidNumberException, NotANumberException {}
+    }
+
+    class Child extends Parent {
+        Child() {}
+
+        Child(int a, int b) {
+            super(a,b);
+        }
+
+        void add(int a, int b) throws InvalidNumberException, NotANumberException {}
+    }
+}
