98692f45386914942024787ff88704f101fb447a
Son-Gyeongi <thsruddl77@gmail.com>
2023-04-27 21:00:42 +0900

    chapter10 SimpleDateFormat 예제2 - SimpleDateFormat의 조상인 DateFormat로 변경

--- src/main/java/ch10/Ex10_9.java
+++ src/main/java/ch10/Ex10_9.java
@@ -1,12 +1,13 @@
 package ch10;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Ex10_9 {
     public static void main(String[] args) {
-        SimpleDateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일");
-        SimpleDateFormat df2 = new SimpleDateFormat("yyyy/MM/dd");
+        DateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일");
+        DateFormat df2 = new SimpleDateFormat("yyyy/MM/dd");
 
         try {
             Date d = df.parse("2023년 4월 27일"); // parse()메서드를 사용해서 문자열을 날짜로 바꾼다.
