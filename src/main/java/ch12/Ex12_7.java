891fb9a64f3b1b01b7d36354922f6b8f6955490b
Son-Gyeongi <thsruddl77@gmail.com>
2023-06-26 17:35:24 +0900

    chapter12 표준 애너테이션 - @Deprecated

--- src/main/java/ch12/Ex12_7.java
+++ src/main/java/ch12/Ex12_7.java
@@ -5,12 +5,14 @@ class Parent {
 }
 
 class Child extends Parent {
-//    @Override // 오버라이딩을 올바르게 했는지 컴파일러가 체크하게 한다.
-//    void parentmethod() {} // 조상의 메서드의 이름을 잘못 적었음
+    @Override // 오버라이딩을 올바르게 했는지 컴파일러가 체크하게 한다.
+    @Deprecated // 앞으로 사용하지 않을 것을 권장하는 필드나 메서드에 붙인다.
+    void parentMethod() {} // 조상의 메서드의 이름을 잘못 적었음
 }
 
 public class Ex12_7 {
     public static void main(String[] args) {
-
+        Child c = new Child();
+        c.parentMethod(); // deprecated된 메서드 사용
     }
 }
