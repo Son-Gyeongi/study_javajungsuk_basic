a4cd5e0f4a3b7dfebe20efefa9335a2176708450
Son-Gyeongi <thsruddl77@gmail.com>
2023-06-29 19:47:07 +0900

    chapter12 Practice12_4 - 지네릭 메서드로 변경

--- src/main/java/ch12/Practice12_4.java
+++ src/main/java/ch12/Practice12_4.java
@@ -3,12 +3,17 @@ package ch12;
 import java.util.ArrayList;
 
 // Ex12_1 참고
+
+/**
+ * 두 개의 ArrayList를 매개변수로 받아서, 하나의 새로운 ArrayList로 병합하는 메서드이다.
+ * 이를 지네릭 메서드로 변경하시오.
+ */
 public class Practice12_4 {
-    public static ArrayList<? extends Product> merge(
-            ArrayList<? extends Product> list,
-            ArrayList<? extends Product> list2
+    public static <T extends Product>ArrayList<T> merge(
+            ArrayList<T>  list,
+            ArrayList<T>  list2
     ) {
-        ArrayList<? extends Product> newList = new ArrayList<>(list);
+        ArrayList<T>  newList = new ArrayList<>(list);
 
         newList.addAll(list2);
 
