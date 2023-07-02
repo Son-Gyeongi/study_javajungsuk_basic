0edeb6ce5c9512ee0b0ea4e1e414312053765422
Son-Gyeongi <thsruddl77@gmail.com>
2023-07-02 17:13:01 +0900

    chapter13 쓰레드 - 쓰레드의 실행제어 (sleep() 예외 처리 메서드)

--- src/main/java/ch13/Ex13_8.java
+++ src/main/java/ch13/Ex13_8.java
@@ -8,13 +8,22 @@ public class Ex13_8 {
         th1.start();
         th2.start();
 
-        try {
-//            th1.sleep(2000); // 2초
-            Thread.sleep(2000); // Thread.sleep();이 바람직
-        } catch (InterruptedException e) {}
+//        try {
+////            th1.sleep(2000); // th1을 2초동안 잠자게 안된다. 다른 쓰레드를 재울 수는 없다.
+//            Thread.sleep(2000); // Thread.sleep();이 바람직
+//        } catch (InterruptedException e) {}
+        // 예외 처리 메서드 호출
+        delay(2 * 1000);
 
         System.out.print("<<main 종료>>");
     } // main
+
+    // 예외 처리 메서드
+    static void delay(long millis) {
+        try {
+            Thread.sleep(millis); // Thread.sleep();이 바람직
+        } catch (InterruptedException e) {}
+    }
 }
 
 class ThreadEx8_1 extends Thread {
