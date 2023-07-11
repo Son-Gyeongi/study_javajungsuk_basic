85f6cc2936ca2f30aacf422e74bbc1c70ccfe004
Son-Gyeongi <thsruddl77@gmail.com>
2023-07-11 20:42:36 +0900

    chapter14 스트림의 중간연산 - peek()

--- src/main/java/ch14/Ex14_6.java
+++ src/main/java/ch14/Ex14_6.java
@@ -3,7 +3,9 @@ package ch14;
 import java.io.File;
 import java.util.stream.Stream;
 
-/* 스트림의 중간연산 - map() */
+/* 스트림의 중간연산 - 
+    map() : 스트림의 요소에 저장된 값 중에서 원하는 필드만 뽑아내거나 특정 형태로 변환해야 할 때 사용,
+    peek() : 연산과 연산 사이에 올바르게 처리되었는지 확인 */
 public class Ex14_6 {
     public static void main(String[] args) {
         File[] fileArr = {
@@ -24,10 +26,12 @@ public class Ex14_6 {
 
         fileStream.map(File::getName) // Stream<File> -> Stream<String>
                 .filter(s -> s.indexOf('.') != -1) // 확장자가 없는 것은 제외
+                .peek(s -> System.out.printf("filename=%s%n", s)) // 파일명을 출력한다. 중간 작업 결과 확인
                 .map(s -> s.substring(s.indexOf('.') + 1)) // 확장자만 추출
+                .peek(s -> System.out.printf("extension=%s%n", s)) // 확장자를 출력한다. 중간 작업 결과 확인
                 .map(String::toUpperCase) // 모두 대문자로 변환
                 .distinct() // 중복 제거
-                .forEach(System.out::print); // JAVABAKTXT
+                .forEach(System.out::println); // JAVABAKTXT
 
         System.out.println();
     }
