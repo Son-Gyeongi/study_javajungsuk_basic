e4d6cf2e4b8104c1075fbf408c505250ccb0b03f
Son-Gyeongi <thsruddl77@gmail.com>
2023-07-17 16:05:58 +0900

    chapter14 Practice14_5 (mapToInt())

--- src/main/java/ch14/Practice14_5.java
+++ src/main/java/ch14/Practice14_5.java
@@ -12,6 +12,10 @@ public class Practice14_5 {
         // 스트림 만들기
         Stream<String> strStream = Stream.of(strArr);
 
+        /**
+         * mapToInt()를 사용해서 Stream<String>을 IntStream으로 변환하는 것이 좋다.
+         * IntStream에는 sum(), average(),max(), min()과 같이 편리한 메서드를 가지고 있기 때문이다.
+         */
 //        int sum = strStream.mapToInt(s -> s.length()).sum();
         int sum = strStream.mapToInt(String::length)
 //                .peek(s-> System.out.println("s = " + s)) // 중간 출력
