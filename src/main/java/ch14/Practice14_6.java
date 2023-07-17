819be6c07e1093e8f5ccc08639d38a0b89518259
Son-Gyeongi <thsruddl77@gmail.com>
2023-07-17 16:07:43 +0900

    chapter14 Practice14_6

--- src/main/java/ch14/Practice14_6.java
+++ src/main/java/ch14/Practice14_6.java
@@ -3,6 +3,9 @@ package ch14;
 import java.util.Comparator;
 import java.util.stream.Stream;
 
+/**
+ * 문자열 배열 strArr의 문자열 중에서 가장 긴 것의 길이를 출력하시오.
+ */
 public class Practice14_6 {
     public static void main(String[] args) {
         String[] strArr = {"aaa", "bb", "c", "dddd"};
