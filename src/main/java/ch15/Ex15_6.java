2d19bc74310f14ec06d55ba0ab25cee10349664d
Son-Gyeongi <thsruddl77@gmail.com>
2023-07-23 12:42:32 +0900

    chapter15 edit.BufferedOutputStream 예제

--- src/main/java/ch15/Ex15_6.java
+++ src/main/java/ch15/Ex15_6.java
@@ -14,7 +14,7 @@ public class Ex15_6 {
             // BufferedOutputStream의 버퍼 크기를 5로 한다.
             BufferedOutputStream bos = new BufferedOutputStream(fos, 5);
             // 파일 123.txt에 1부터 9까지 출력한다.
-            for (int i = '1'; i < -'9'; i++) {
+            for (int i = '1'; i <='9'; i++) {
                 bos.write(i);
             }
 
