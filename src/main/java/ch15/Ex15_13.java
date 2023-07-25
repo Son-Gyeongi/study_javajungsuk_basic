9ec4263c2f543ee0d6a451ba73c2809b90a07ad5
Son-Gyeongi <thsruddl77@gmail.com>
2023-07-25 22:25:13 +0900

    chapter15 표준 입출력의 대상변경

--- src/main/java/ch15/Ex15_13.java
+++ src/main/java/ch15/Ex15_13.java
@@ -1,2 +1,13 @@
-package ch15;public class Ex15_13 {
+package ch15;
+
+/**
+ * 표준 입출력의 대상변경
+ * System.out, System.err 모두 출력대상이 콘솔이기 떄문에
+ * System.out 대신 System.err을 사용해도 같은 결괄르 얻는다.
+ */
+public class Ex15_13 {
+    public static void main(String[] args) {
+        System.out.println("out : Hello World!");
+        System.err.println("err : Hello World!");
+    }
 }
