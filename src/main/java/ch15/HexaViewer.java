c0a06ca9fd170785d4d1246b8266d2fcab9480d7
Son-Gyeongi <thsruddl77@gmail.com>
2023-07-28 23:43:23 +0900

    chapter15 Practice15_2

---
+++ src/main/java/ch15/HexaViewer.java
@@ -0,0 +1,36 @@
+package ch15;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+// Practice15_2
+public class HexaViewer {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("USAGE: java HexaViewer FILENAME");
+            System.exit(0); // 프로그램 정상 종료
+        }
+
+        String inputFile = args[0];
+
+        try {
+            FileInputStream input = new FileInputStream(inputFile);
+            PrintStream output = new PrintStream(System.out);
+
+            int data = 0;
+            int i = 0;
+
+            while ((data = input.read()) != -1) {
+                output.printf("%02x", data);
+                if (++i % 16 == 0)
+                    output.println();
+            }
+
+            input.close();
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } // main
+}
