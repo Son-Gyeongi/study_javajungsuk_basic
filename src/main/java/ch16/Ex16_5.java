088f7cd3365512d94a0e0f234429ac26ebd40ec2
Son-Gyeongi <thsruddl77@gmail.com>
2023-08-01 23:46:56 +0900

    chapter16 URLConnection 클래스 예제3

---
+++ src/main/java/ch16/Ex16_5.java
@@ -0,0 +1,31 @@
+package ch16;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+// URLConnection 클래스 예제3
+public class Ex16_5 {
+    public static void main(String[] args) {
+        URL url = null;
+        InputStream in = null;
+        FileOutputStream out = null;
+        String address = "http://www.codechobo.com/main";
+
+        int ch = 0;
+
+        try {
+            url = new URL(address);
+            in = url.openStream();
+            out = new FileOutputStream("");
+
+            while ((ch = in.read()) != -1) {
+                out.write(ch);
+            }
+            in.close();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } // main
+}
