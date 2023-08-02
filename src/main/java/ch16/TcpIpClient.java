242dff387a17bac5015cd10160e3d41b208305d4
Son-Gyeongi <thsruddl77@gmail.com>
2023-08-02 23:50:26 +0900

    chapter16 TCP 소켓 프로그래밍 예제2

---
+++ src/main/java/ch16/TcpIpClient.java
@@ -0,0 +1,39 @@
+package ch16;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ConnectException;
+import java.net.Socket;
+
+// Ex16_7 TCP 소켓 프로그래밍 예제2
+public class TcpIpClient {
+    public static void main(String[] args) {
+        try {
+            String serverIp = "127.0.0.1";
+
+            System.out.println("서버에 연결중입니다. 서버IP : " + serverIp);
+            // 소켓을 생성하여 연결을 요청한다.
+            Socket socket = new Socket(serverIp, 8080);
+
+            // 소켓의 입력스트림을 얻는다.
+            InputStream in = socket.getInputStream();
+            DataInputStream dis = new DataInputStream(in);
+
+            // 소켓으로부터 받은 데이터를 출력한다.
+            System.out.println("서버로부터 받은 메시지 : " + dis.readUTF());
+            System.out.println("연결을 종료합니다.");
+
+            // 스트립과 소켓을 닫는다.
+            dis.close();
+            socket.close();
+            System.out.println("연결이 종료되었습니다.");
+        } catch (ConnectException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } // main
+}
